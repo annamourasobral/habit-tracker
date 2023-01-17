a736caa4c9a3cacec35f25ffef584d9c40b07164
Anna Moura Sobral <annamourasobral@gmail.com>
2023-01-17 20:00:34 +0100

    mobile setup

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,17 @@
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+export function Loading() {
+    return (
+        <View style={styles.loading}>
+            <ActivityIndicator color='#7C3AED' />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    loading: {
+        flex: 1,
+        backgroundColor: '#09090A',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
